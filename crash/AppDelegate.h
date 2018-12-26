08149120866cafe81e8c7bd98609216274e42b9e
liuyujia <417378336@qq.com>
2018-12-26 17:08:18 +0800

    Initial Commit

---
+++ crash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  crash
+//
+//  Created by liuyujia on 2018/12/26.
+//  Copyright © 2018 liuyujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crash/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  crash
+//
+//  Created by liuyujia on 2018/12/26.
+//  Copyright © 2018 liuyujia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
